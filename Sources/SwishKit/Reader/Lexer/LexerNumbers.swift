ebfb3c785e90bcefa9a6d2947cc0ba5a186c1bd7
Rod Schmidt <rod@infinitenil.com>
2026-04-14 16:09:34 -0600

    Improve scanDecimalNumber

--- Sources/SwishKit/Reader/Lexer/LexerNumbers.swift
+++ Sources/SwishKit/Reader/Lexer/LexerNumbers.swift
@@ -1,138 +1,39 @@
 extension Lexer {
     func scanDecimalNumber(startLine: Int, startColumn: Int, prefix: String = "") throws -> Token {
         var text = prefix
-        var lastWasUnderscore = false
         var isFloat = false
 
-        // Scan integer part
-        while let char = peek() {
-            if char.isNumber {
-                text.append(advance())
-                lastWasUnderscore = false
-            }
-            else if char == "_" {
-                if lastWasUnderscore {
-                    throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
-                }
-                advance()
-                text.append("_")
-                lastWasUnderscore = true
-            }
-            else {
-                break
-            }
-        }
-
-        if lastWasUnderscore {
-            throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
-        }
+        try scanDigitSequence(into: &text, isDigit: { $0.isNumber }, startLine: startLine, startColumn: startColumn)
 
         // Check for ratio: / followed by digit
-        if let slash = peek(), slash == "/", let afterSlash = peekAt(1), afterSlash.isNumber {
+        if peek() == "/", let afterSlash = peekAt(1), afterSlash.isNumber {
             text.append(advance()) // consume '/'
-            lastWasUnderscore = false
-
-            while let char = peek() {
-                if char.isNumber {
-                    text.append(advance())
-                    lastWasUnderscore = false
-                }
-                else if char == "_" {
-                    if lastWasUnderscore {
-                        throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
-                    }
-                    advance()
-                    text.append("_")
-                    lastWasUnderscore = true
-                }
-                else {
-                    break
-                }
-            }
-
-            if lastWasUnderscore {
-                throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
-            }
-
+            try scanDigitSequence(into: &text, isDigit: { $0.isNumber }, startLine: startLine, startColumn: startColumn)
             try validateNumberEnd(text: text, startLine: startLine, startColumn: startColumn)
             let cleanText = text.filter { $0 != "_" }
-
             let parts = cleanText.split(separator: "/", maxSplits: 1)
             if parts.count == 2, let denom = Int(String(parts[1])), denom == 0 {
                 throw LexerError.invalidRatio(cleanText, line: startLine, column: startColumn)
             }
-
             return Token(type: .ratio, text: cleanText, line: startLine, column: startColumn)
         }
 
         // Check for fractional part: . followed by digit
-        if let dot = peek(), dot == ".", let afterDot = peekAt(1), afterDot.isNumber {
+        if peek() == ".", let afterDot = peekAt(1), afterDot.isNumber {
             isFloat = true
             text.append(advance()) // consume '.'
-            lastWasUnderscore = false
-
-            while let char = peek() {
-                if char.isNumber {
-                    text.append(advance())
-                    lastWasUnderscore = false
-                }
-                else if char == "_" {
-                    if lastWasUnderscore {
-                        throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
-                    }
-                    advance()
-                    text.append("_")
-                    lastWasUnderscore = true
-                }
-                else {
-                    break
-                }
-            }
-
-            if lastWasUnderscore {
-                throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
-            }
+            try scanDigitSequence(into: &text, isDigit: { $0.isNumber }, startLine: startLine, startColumn: startColumn)
         }
 
         // Check for exponent: e or E
         if let e = peek(), e == "e" || e == "E" {
             var offset = 1
-            if let sign = peekAt(offset), sign == "+" || sign == "-" {
-                offset += 1
-            }
+            if let sign = peekAt(offset), sign == "+" || sign == "-" { offset += 1 }
             if let digitAfter = peekAt(offset), digitAfter.isNumber {
                 isFloat = true
                 text.append(advance()) // consume 'e' or 'E'
-
-                if let sign = peek(), sign == "+" || sign == "-" {
-                    text.append(advance())
-                }
-
-                lastWasUnderscore = false
-                var hasExponentDigits = false
-
-                while let char = peek() {
-                    if char.isNumber {
-                        text.append(advance())
-                        hasExponentDigits = true
-                        lastWasUnderscore = false
-                    }
-                    else if char == "_" {
-                        if !hasExponentDigits || lastWasUnderscore {
-                            throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
-                        }
-                        advance()
-                        text.append("_")
-                        lastWasUnderscore = true
-                    }
-                    else {
-                        break
-                    }
-                }
-
-                if lastWasUnderscore {
-                    throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
-                }
+                if let sign = peek(), sign == "+" || sign == "-" { text.append(advance()) }
+                try scanDigitSequence(into: &text, isDigit: { $0.isNumber }, startLine: startLine, startColumn: startColumn)
             }
         }
 
@@ -145,33 +46,7 @@ extension Lexer {
         var text = prefix
         text.append(advance()) // '0'
         text.append(advance()) // 'x'
-
-        var hasDigits = false
-        var lastWasUnderscore = false
-
-        while let char = peek() {
-            if isHexDigit(char) {
-                text.append(advance())
-                hasDigits = true
-                lastWasUnderscore = false
-            }
-            else if char == "_" {
-                if !hasDigits || lastWasUnderscore {
-                    throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
-                }
-                advance()
-                text.append("_")
-                lastWasUnderscore = true
-            }
-            else {
-                break
-            }
-        }
-
-        if !hasDigits || lastWasUnderscore {
-            throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
-        }
-
+        try scanDigitSequence(into: &text, isDigit: isHexDigit, requiresLeadingDigit: true, startLine: startLine, startColumn: startColumn)
         try validateNumberEnd(text: text, startLine: startLine, startColumn: startColumn)
         let cleanText = text.filter { $0 != "_" }
         return Token(type: .integer, text: cleanText, line: startLine, column: startColumn)
@@ -181,33 +56,7 @@ extension Lexer {
         var text = prefix
         text.append(advance()) // '0'
         text.append(advance()) // 'b'
-
-        var hasDigits = false
-        var lastWasUnderscore = false
-
-        while let char = peek() {
-            if isBinaryDigit(char) {
-                text.append(advance())
-                hasDigits = true
-                lastWasUnderscore = false
-            }
-            else if char == "_" {
-                if !hasDigits || lastWasUnderscore {
-                    throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
-                }
-                advance()
-                text.append("_")
-                lastWasUnderscore = true
-            }
-            else {
-                break
-            }
-        }
-
-        if !hasDigits || lastWasUnderscore {
-            throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
-        }
-
+        try scanDigitSequence(into: &text, isDigit: isBinaryDigit, requiresLeadingDigit: true, startLine: startLine, startColumn: startColumn)
         try validateNumberEnd(text: text, startLine: startLine, startColumn: startColumn)
         let cleanText = text.filter { $0 != "_" }
         return Token(type: .integer, text: cleanText, line: startLine, column: startColumn)
@@ -217,35 +66,44 @@ extension Lexer {
         var text = prefix
         text.append(advance()) // '0'
         text.append(advance()) // 'o'
+        try scanDigitSequence(into: &text, isDigit: isOctalDigit, requiresLeadingDigit: true, startLine: startLine, startColumn: startColumn)
+        try validateNumberEnd(text: text, startLine: startLine, startColumn: startColumn)
+        let cleanText = text.filter { $0 != "_" }
+        return Token(type: .integer, text: cleanText, line: startLine, column: startColumn)
+    }
 
+    /// Scans a sequence of digits (determined by `isDigit`) and underscores into `text`.
+    /// Rejects consecutive underscores and a trailing underscore.
+    /// When `requiresLeadingDigit` is true, also rejects an empty sequence (e.g. `0x_`).
+    private func scanDigitSequence(
+        into text: inout String,
+        isDigit: (Character) -> Bool,
+        requiresLeadingDigit: Bool = false,
+        startLine: Int,
+        startColumn: Int
+    ) throws {
         var hasDigits = false
         var lastWasUnderscore = false
 
         while let char = peek() {
-            if isOctalDigit(char) {
+            if isDigit(char) {
                 text.append(advance())
                 hasDigits = true
                 lastWasUnderscore = false
-            }
-            else if char == "_" {
+            } else if char == "_" {
                 if !hasDigits || lastWasUnderscore {
                     throw LexerError.invalidNumberFormat(text + "_", line: startLine, column: startColumn)
                 }
                 advance()
                 text.append("_")
                 lastWasUnderscore = true
-            }
-            else {
+            } else {
                 break
             }
         }
 
-        if !hasDigits || lastWasUnderscore {
+        if (requiresLeadingDigit && !hasDigits) || lastWasUnderscore {
             throw LexerError.invalidNumberFormat(text, line: startLine, column: startColumn)
         }
-
-        try validateNumberEnd(text: text, startLine: startLine, startColumn: startColumn)
-        let cleanText = text.filter { $0 != "_" }
-        return Token(type: .integer, text: cleanText, line: startLine, column: startColumn)
     }
 }
