7c6564881d2a080b1449a2edac48bfbc9eb16905
Rod Schmidt <rod@infinitenil.com>
2026-05-13 16:42:24 -0600

    Add comment macro

--- Sources/SwishKit/Resources/clojure/core.clj
+++ Sources/SwishKit/Resources/clojure/core.clj
@@ -1,4 +1,9 @@
 (ns clojure.core)
 
+(defmacro comment
+  "Ignores body, yields nil"
+  ;{:added "1.0"}
+  [& body])
+
 (defmacro defn [name params & body]
   `(def ~name (fn ~name ~params ~@body)))
