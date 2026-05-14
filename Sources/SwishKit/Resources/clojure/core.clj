7a77d9896e91360b369965f7c1bb3cb582516652
Rod Schmidt <rod@infinitenil.com>
2026-05-13 18:40:16 -0600

    Add when macro to core

--- Sources/SwishKit/Resources/clojure/core.clj
+++ Sources/SwishKit/Resources/clojure/core.clj
@@ -14,3 +14,9 @@
   ; :added "1.0"
   ; :static true}
   [x] (if x false true))
+
+(defmacro when
+  "Evaluates test. If logical true, evaluates body in an implicit do."
+  ;{:added "1.0"}
+  [test & body]
+  (list 'if test (cons 'do body)))
