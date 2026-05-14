da3892eea4e0b31e587b7fc6f0798703a05b1dc7
Rod Schmidt <rod@infinitenil.com>
2026-05-13 18:07:06 -0600

    Add not function to clojure.core

--- Sources/SwishKit/Resources/clojure/core.clj
+++ Sources/SwishKit/Resources/clojure/core.clj
@@ -7,3 +7,10 @@
 
 (defmacro defn [name params & body]
   `(def ~name (fn ~name ~params ~@body)))
+
+(defn not
+  ;"Returns true if x is logical false, false otherwise."
+  ;{:tag Boolean
+  ; :added "1.0"
+  ; :static true}
+  [x] (if x false true))
